d62bc9b10cf41bbe446a77e1343be726f4096bc1
OscarKing888 <oscar.ken@gmail.com>
2023-04-16 14:30:45 +0800

    fix setup.bat error

--- Resnet/setup.bat
+++ Resnet/setup.bat
@@ -7,13 +7,17 @@ echo Activating the "resnet" environment...
 call conda activate resnet
 echo.
 
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+echo.
+
+
 echo Installing PyTorch 2.0.0 (CUDA 11.8 version)...
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 echo.
 
-echo Installing dependencies from requirements.txt...
-pip install -r requirements.txt
-echo.
+conda install pytorch torchvision -c pytorch
+
 
 echo Environment setup completed.
 pause

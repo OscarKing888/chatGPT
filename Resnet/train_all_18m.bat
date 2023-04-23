b1bb845be1ac4c3bda92930954708d4b838c7fab
oscar <oscar.ken@gmail.com>
2023-04-23 20:50:53 +0800

    add more CIFAR10 train bat

--- Resnet/train_all_18m.bat
+++ Resnet/train_all_18m.bat
@@ -1,9 +1,11 @@
 @echo on
-set logdir=logs18mstl10
+
+set dataset=CIFAR10
+
+set logdir=logs\18m_%dataset%
 mkdir %logdir%
 
 
-set dataset=STL10
 set scheduler=--scheduler
 
 set batchsize=32
@@ -17,5 +19,40 @@ call train18m.bat --dataset %dataset% --batchsize %batchsize% %scheduler% > %log
 set batchsize=128
 echo Training %dataset% batch_size:%batchsize%
 call train18m.bat --dataset %dataset% --batchsize %batchsize% %scheduler% > %logdir%\%dataset%_%batchsize%.log
+
+
+set batchsize=256
+echo Training %dataset% batch_size:%batchsize%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% %scheduler% > %logdir%\%dataset%_%batchsize%.log
+
+
+set batchsize=512
+echo Training %dataset% batch_size:%batchsize%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% %scheduler% > %logdir%\%dataset%_%batchsize%.log
+
+
+set scheduler=""
+
+set batchsize=32
+echo Training %dataset% batch_size:%batchsize% scheduler:%scheduler%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% > %logdir%\%dataset%_%batchsize%.log
+
+set batchsize=64
+echo Training %dataset% batch_size:%batchsize%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% > %logdir%\%dataset%_%batchsize%.log
+
+set batchsize=128
+echo Training %dataset% batch_size:%batchsize%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% > %logdir%\%dataset%_%batchsize%.log
+
+
+set batchsize=256
+echo Training %dataset% batch_size:%batchsize%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% > %logdir%\%dataset%_%batchsize%.log
+
+
+set batchsize=512
+echo Training %dataset% batch_size:%batchsize%
+call train18m.bat --dataset %dataset% --batchsize %batchsize% > %logdir%\%dataset%_%batchsize%.log
 
 echo All trainings completed!

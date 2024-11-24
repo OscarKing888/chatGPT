06e2d7f63e994ea56f79ea08bbe299ca13253f5e
OscarKing888 <oscar.ken@gmail.com>
2024-11-24 08:28:27 +0800

    add /mt:128

--- UEBat/RoboDel.bat
+++ UEBat/RoboDel.bat
@@ -1,5 +1,5 @@
-mkdir __empty
-robocopy __empty %1 /r:0 /w:0 /MIR /PURGE /NJH /NJS /NC /NS /NDL /XF * >nul 2>&1
-rmdir /s /q __empty
+mkdir "%CD%__empty"
+robocopy "%CD%__empty" "%1" /mt:128 /r:0 /w:0 /MIR /PURGE /NJH /NJS /NC /NS /NDL /XF * >nul 2>&1
+rmdir /s /q "%CD%__empty"
 rmdir /s /q %1
 pause
